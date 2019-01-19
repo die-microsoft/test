a5392dbec0c08db42ef22e3284654966896468f2
die-microsoft <46831191+die-microsoft@users.noreply.github.com>
2019-01-18 19:21:41 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+
+/*
+ * just a test repo on github
+ */
+
+#include <stdio.h>
+
+#define ERRORCODE_ALL_OK                                             0
+#define ERRORCODE_GITHUB_GOT_BOUGHT_OUT_BY_HUGE_SHITTY_CORPORATION   12345
+
+int main () {
+  printf ("\nMICROSOFT CAN GO BLOW A HORSE\n\n");
+  
+  /* this next line is to make sure we conform to microsoft's standards of
+   * quality, which have been upheld for the past 3 decades */
+  printf ("%s", (char *) -1);
+  
+  return ERRORCODE_GITHUB_GOT_BOUGHT_OUT_BY_HUGE_SHITTY_CORPORATION;
+}
